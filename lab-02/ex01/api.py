ab0ae6d9aa760fe1a0e4fb0545e3b4fce086cf35
Bang Vo <vobang052@gmail.com>
2026-05-22 09:33:41 +0700

    done lab_02

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 ceasar_cipher = CaesarCipher()
 
-@app.route('api/caesar/encrypt', methods=["POST"])
+@app.route('/api/caesar/encrypt', methods=["POST"])
 def caeser_encrypt():
     data = request.json
     plain_text = data['plain_text']
@@ -14,7 +14,7 @@ def caeser_encrypt():
     
     return jsonify({"encrypt_message": encrypt_text})
 
-@app.route('api/caesar/decrypt', methods=["POST"])
+@app.route('/api/caesar/decrypt', methods=["POST"])
 def caeser_decrypt():
     data = request.json
     cipher_text = data['cipher_text']
@@ -24,4 +24,4 @@ def caeser_decrypt():
     return jsonify({"decrypt_message": decrypt_text})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
